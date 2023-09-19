dd5bc54e1546d9961f445adf9b08bec0ac738711
Bruno Faria <bruno.fariadevl@gmail.com>
2023-09-19 20:59:15 -0300

    atualizando modelo do medico, pois ele tem uma categoria como dermatologista

--- client/src/models/DoctorUser.d.ts
+++ client/src/models/DoctorUser.d.ts
@@ -2,6 +2,7 @@ export interface DoctorUser {
     name: string
     email: string
     password: string
+    doctor_category: string
     doctorClinic: DoctorClinic
     userType: string
 }
--- server/src/models/Doctor.d.ts
+++ server/src/models/Doctor.d.ts
@@ -1,5 +1,7 @@
 interface Doctor {
   name: string
   email: string
+  doctor_category: string
   coordinate: Coordinates
+  userType: string
 }
