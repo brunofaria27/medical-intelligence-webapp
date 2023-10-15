fe4cf04cbf65374d6d7262442cc8c5e3b6572403
Bruno Faria <bruno.fariadevl@gmail.com>
2023-10-15 01:22:19 -0300

    API para mandar a classificação pronta, falta so treinar mais

--- server-ai/main.py
+++ server-ai/main.py
@@ -5,10 +5,11 @@ from werkzeug.utils import secure_filename
 import os
 import sys
 
+from processment import process_image
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './uploads'
 cors = CORS(app, support_credentials=True)
-
 
 @app.route('/predict', methods=['POST'])
 @cross_origin(supports_credentials=True)
@@ -21,11 +22,17 @@ def image_upload():
         print('No selected file')
         return jsonify({'message': 'No selected file'}), 400
     if file:
+        CLASS_MAPPING = {0: 'Actinic keratosis', 1: 'Basal cell carcinoma', 2: 'Benign keratosis', 3: 'Dermatofibroma', 4: 'Melanocytic nevus', 5: 'Melanoma', 6: 'Squamous cell carcinoma', 7: 'Vascular lesion'}
+        IMAGE_SIZE = (224, 224)
+        MODEL_PATH = './treined-model/skin_disease_model.h5'
+
         filename = secure_filename(file.filename)
+        image_path = f'./uploads/{filename}'
         print(f'File received: {filename}')
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # TODO: Aqui botar a função para classificar a imagem enviada e trocar o retorno para o resultado da classificação
-        return jsonify({'message': 'File received and saved'}), 200
+        classification = process_image(image_path, MODEL_PATH, IMAGE_SIZE, CLASS_MAPPING)
+        os.remove(f'./uploads/{filename}')
+        return jsonify({'classification': classification}), 200
 
 if __name__ == '__main__':
     app.run()
